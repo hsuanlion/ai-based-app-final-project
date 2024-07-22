db8a1a19fad6cc895abd78ca2bb8215207b98c6b
hsuanlion <hsuanleelion@gmail.com>
2024-07-21 22:01:38 -0400

    add server.py

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route('/emotionDetector')
+def emotionDetector():
+    
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = emotion_detector(text_to_analyze)
+
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    response_text = f""For the given statement, the system response is 'anger' \
+    : {anger}, 'disgust' : {disgust}, 'fear' : {fear}, 'joy' : {'joy'} \
+    and 'sadness' : {sadness}. The dominant emotion is {dominant_emotion}.""
+
+    return render_template('index', response_text = response_text)
+
+
+if __name__ == '__main__':
+    app.run(host = "localhost", post = 5000)
--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -4,18 +4,18 @@ import unnittest
 class TestEmotionDetector(unnittest.TestCase):
     def test_emotion_detector(self):
         result1 = emotion_detector("I am glad this happened")
-        self.assertEqual(result1, "joy")
+        self.assertEqual(result1['dominant_emotion'], "joy")
 
         result2 = emotion_detector("I am really mad about this")
-        self.assertEqual(result2, "joy")
+        self.assertEqual(result2['dominant_emotion'], "joy")
 
         result3 = emotion_detector("I feel disgusted just hearing about this")
-        self.assertEqual(result3, "disgust")
+        self.assertEqual(result3['dominant_emotion'], "disgust")
 
         result4 = emotion_detector("I am so sad about this")
-        self.assertEqual(result4, "sadness")
+        self.assertEqual(result4['dominant_emotion'], "sadness")
 
         result5 = emotion_detector("I am really afraid that this will happen")
-        self.assertEqual(result5, "fear")
+        self.assertEqual(result5['dominant_emotion'], "fear")
 
 unnittest.main()
