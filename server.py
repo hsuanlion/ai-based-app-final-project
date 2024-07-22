20d9ed36f7e87dafed15e0d6f21265a32937f67d
hsuanlion <hsuanleelion@gmail.com>
2024-07-21 22:34:33 -0400

    static code analysis - server.py

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
+'''
+    docstring
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route('/emotionDetector')
-def emotionDetector():
-    
+def emotion_detect():
+    '''
+    docstring
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
@@ -21,7 +26,7 @@ def emotionDetector():
         response_text = "Invalid text! Please try again!"
     else:
         response_text = f"""For the given statement, the system response is 'anger' \
-        : {anger}, 'disgust' : {disgust}, 'fear' : {fear}, 'joy' : {'joy'} \
+        : {anger}, 'disgust' : {disgust}, 'fear' : {fear}, 'joy' : {joy} \
         and 'sadness' : {sadness}. The dominant emotion is {dominant_emotion}."""
 
     return render_template('index', system_response = response_text)
